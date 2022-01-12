d27d35a4d11ef88501f0a223a477f55637cf49c2
Danny <danielswift1613@gmail.com>
2022-01-12 20:11:29 +1100

    asd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-
+    var page_url = window.location.href;
+    var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+    if (page_id == "section3") {
+        $("html, body").animate({
+            scrollTop: $("#scroll-" + page_id).offset().top
+        }, 1000);
+    } else if (page_id == "post") {
+        $("html, body").animate({
+            scrollTop: $("#scroll-" + page_id).offset().top
+        }, 1000);
+    }
 });
+
+
+let viewLoginPwd = false;
+
+function changePwdView() {
+    let getPwdView = document.getElementById("viewPwdLogin");
+
+    if (viewLoginPwd === false) {
+        getPwdView.setAttribute("type", "text");
+        viewLoginPwd = true;
+    } else if (viewLoginPwd === true) {
+        getPwdView.setAttribute("type", "password");
+        viewLoginPwd = false;
+    }
+}
